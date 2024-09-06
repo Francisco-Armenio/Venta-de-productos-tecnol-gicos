e123747bc202587210ebf3ff028742d8e90e8614
francisco <franciscoarmenio1@gmail.com>
2024-09-05 23:39:11 -0300

    incluyendo array

--- carpeta js/app.js
+++ carpeta js/app.js
@@ -1,3 +1,163 @@
+const productos = [
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: ""
+    },
+    {
+        id: "",
+        titulo: "",
+        imagen: "",
+        categoria:{
+            nombre: "",
+            id: ""
+        },
+        precio: "",
+        precio2:""
+    },
 
+];
 
 
